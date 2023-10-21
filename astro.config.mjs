c43f7eb335bf15b4e457f3f92b6d0cadcd80801d
Raigiku <raigiku@hotmail.com>
2023-10-21 21:00:39 +0200

    change from ssr to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: "server",
+  output: "static",
   adapter: vercel()
 });
