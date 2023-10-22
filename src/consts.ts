2441ed4979c18cec0356153590f8a9a502b64165
Raigiku <raigiku@hotmail.com>
2023-10-22 19:50:18 +0200

    remove about page, add website description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Diego Coding Tech';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = `Welcome to my blog. I share my thoughts on how to build quality software, with an emphasis on software design and architecture. By laying out my thoughts and ideas, I intend to establish a set of standard rules that will guide me in developing maintainable software. I hope you benefit from my writings! Feel free to share your own insights and knowledge. I'm eager to learn from you as well!`;
